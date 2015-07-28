e55ed8e01e6f4802d1c1f0f9c8c947d754145729
DKWoods <david@spurgeonwoods.com>
2015-07-28 15:27:31 -0500

    Transana 2.99a7 - Update 1
    Updated version number, integrated Jen's edits to the Tutorial

--- TransanaConstants.py
+++ TransanaConstants.py
@@ -53,9 +53,9 @@ if workshopVersion:
     expirationdate = TransanaConfigConstants.xpdt
 
 # Program Version Number
-versionNumber = '2.99 Alpha 6'
+versionNumber = '2.99 Alpha 7'
 # Build Number
-buildNumber = '299a6'
+buildNumber = '299a7'
 # Modify for Multi-user if appropriate
 if not singleUserVersion:
     versionNumber = versionNumber + '-MU'
